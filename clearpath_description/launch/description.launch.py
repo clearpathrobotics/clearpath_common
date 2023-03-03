568459574b4cf1ae4b40ba4af421d8679670fdc7
Tony Baltovski <tbaltovski@clearpathrobotics.com>
2023-03-03 09:46:33 -0500

    Initial clearpath_description with only Husky.

---
+++ clearpath_description/launch/description.launch.py
@@ -0,0 +1,35 @@
+
+from launch import LaunchDescription
+from launch.substitutions import Command, FindExecutable, PathJoinSubstitution
+from launch_ros.actions import Node
+from launch_ros.substitutions import FindPackageShare
+
+import xacro
+
+
+def generate_launch_description():
+
+    # Get URDF via xacro
+    robot_description_content = Command(
+        [
+            PathJoinSubstitution([FindExecutable(name="xacro")]),
+            " ",
+            PathJoinSubstitution(
+                [FindPackageShare("clearpath_description"), "urdf", "husky/husky.urdf.xacro"]
+            ),
+        ]
+    )
+    robot_description = {"robot_description": robot_description_content}
+
+    node_robot_state_publisher = Node(
+        package="robot_state_publisher",
+        executable="robot_state_publisher",
+        output="screen",
+        parameters=[robot_description],
+    )
+
+    return LaunchDescription(
+        [
+            node_robot_state_publisher,
+        ]
+    )
