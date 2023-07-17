0cd1838ff1416d3306cc02af92495b64357c576b
fazzrazz <danielduranrojas@gmail.com>
2023-07-17 10:19:50 -0400

    Changes whitespace

--- clearpath_generator_common/clearpath_generator_common/description/sensors.py
+++ clearpath_generator_common/clearpath_generator_common/description/sensors.py
@@ -161,17 +161,7 @@ class SensorDescription():
                 self.IMAGE_HEIGHT: sensor.get_color_height(),
                 self.IMAGE_WIDTH: sensor.get_color_width(),
             })
-    
-    class FlirBlackflyDescription(CameraDescription):
-        CONNECTION_TYPE = 'connection_type'
 
-        def __init__(self, sensor: FlirBlackfly) -> None:
-            super().__init__(sensor)
-
-            self.parameters.update({
-                self.CONNECTION_TYPE: sensor.get_connection_type()
-            })
-    
     MODEL = {
         HokuyoUST10.SENSOR_MODEL: Lidar2dDescription,
         SickLMS1XX.SENSOR_MODEL: Lidar2dDescription,
@@ -179,7 +169,7 @@ class SensorDescription():
         Microstrain.SENSOR_MODEL: ImuDescription,
         VelodyneLidar.SENSOR_MODEL: Lidar3dDescription,
         SwiftNavDuro.SENSOR_MODEL: BaseDescription,
-        FlirBlackfly.SENSOR_MODEL: FlirBlackflyDescription
+        FlirBlackfly.SENSOR_MODEL: CameraDescription
     }
 
     def __new__(cls, sensor: BaseSensor) -> BaseDescription:
