a2c82f75d2e3fd9e89de9d37f4ee775b4a91673b
Abed Al Rahman Al Mrad <aalmrad@kinova.ca>
2024-09-26 13:39:06 -0400

    Removed the non-functional moveit package

--- clearpath_moveit_config/launch/static_virtual_joint_tfs.launch.py
+++
@@ -1,7 +0,0 @@
-from moveit_configs_utils import MoveItConfigsBuilder
-from moveit_configs_utils.launches import generate_static_virtual_joint_tfs_launch
-
-
-def generate_launch_description():
-    moveit_config = MoveItConfigsBuilder("a200-0000", package_name="clearpath_moveit_config").to_moveit_configs()
-    return generate_static_virtual_joint_tfs_launch(moveit_config)
