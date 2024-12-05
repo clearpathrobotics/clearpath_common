9800c21a13a960dbfd7cdfc3efa62e2294833ff6
Chris Iverach-Brereton <civerachb@clearpathrobotics.com>
2024-12-05 13:07:30 -0500

    Catch unsupported platforms/accessories in vcan generation

--- clearpath_generator_common/test/test_generator_vcan.py
+++ clearpath_generator_common/test/test_generator_vcan.py
@@ -30,6 +30,8 @@ import shutil
 
 from ament_index_python.packages import get_package_share_directory
 from clearpath_generator_common.vcan.generator import VirtualCANGenerator
+from clearpath_config.common.types.exception import UnsupportedAccessoryException
+from clearpath_config.common.types.exception import UnsupportedPlatformException
 
 
 class TestRobotLaunchGenerator:
@@ -49,6 +51,10 @@ class TestRobotLaunchGenerator:
             try:
                 rlg = VirtualCANGenerator(os.path.dirname(dst))
                 rlg.generate()
+            except UnsupportedAccessoryException as e:
+                print(f'Unsupported accessory: {e}. Skipping')
+            except UnsupportedPlatformException as e:
+                print(f'Unsupported platform: {e}. Skipping')
             except Exception as e:
                 errors.append("Sample '%s' failed to load: '%s'" % (
                     sample,
