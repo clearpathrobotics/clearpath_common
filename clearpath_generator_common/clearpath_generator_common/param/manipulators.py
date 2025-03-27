1bd2953b121b0bb459573bbe1d1cb4370ae15f06
Luis Camero <lcamero@clearpathrobotics.com>
2025-03-27 13:29:49 -0400

    Resolve formatting issues

--- clearpath_generator_common/clearpath_generator_common/param/manipulators.py
+++ clearpath_generator_common/clearpath_generator_common/param/manipulators.py
@@ -78,10 +78,13 @@ class ManipulatorParam():
                 package=self.default_parameter_package,
                 path=self.default_parameter_directory,
             )
-            namespace = self.namespace if self.namespace != '/' else ''
+            if self.namespace == '/':
+                namespace = ''
+            else:
+                namespace = self.namespace
             self.param_file = ParamFile(
                 name=self.default_parameter_name,
-                namespace=namespace + '/manipulators',
+                namespace=f'{namespace}/manipulators',
                 path=self.param_path
             )
 
