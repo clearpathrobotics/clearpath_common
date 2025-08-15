7853c559cbecb38f900b11b1f77a5fc7ef4dfe38
Roni Kreinin <rkreinin@clearpathrobotics.com>
2025-08-15 11:03:15 -0400

    Linting

--- clearpath_diagnostics/src/clearpath_diagnostic_updater.cpp
+++ clearpath_diagnostics/src/clearpath_diagnostic_updater.cpp
@@ -54,7 +54,8 @@ ClearpathDiagnosticUpdater::ClearpathDiagnosticUpdater()
   mcu_status_topic_ = get_string_param("mcu_status_topic");
   mcu_status_topic_ = (mcu_status_topic_ == UNKNOWN) ? "platform/mcu/status" : mcu_status_topic_;
   mcu_alerts_topic_ = get_string_param("mcu_alerts_topic");
-  mcu_alerts_topic_ = (mcu_alerts_topic_ == UNKNOWN) ? "platform/mcu/status/alerts" : mcu_alerts_topic_;
+  mcu_alerts_topic_ = (mcu_alerts_topic_ ==
+    UNKNOWN) ? "platform/mcu/status/alerts" : mcu_alerts_topic_;
   mcu_power_topic_ = get_string_param("mcu_power_topic");
   mcu_power_topic_ = (mcu_power_topic_ == UNKNOWN) ? "platform/mcu/status/power" : mcu_power_topic_;
   bms_state_topic_ = get_string_param("bms_state_topic");
@@ -114,7 +115,8 @@ ClearpathDiagnosticUpdater::ClearpathDiagnosticUpdater()
 
     // Add diagnostic tasks for MCU data
     updater_.add("MCU Status", this, &ClearpathDiagnosticUpdater::mcu_status_diagnostic);
-    updater_.add("MCU Firmware Alerts", this, &ClearpathDiagnosticUpdater::firmware_alerts_diagnostic);
+    updater_.add("MCU Firmware Alerts", this,
+      &ClearpathDiagnosticUpdater::firmware_alerts_diagnostic);
     updater_.add("MCU Firmware Version", this, &ClearpathDiagnosticUpdater::firmware_diagnostic);
     RCLCPP_INFO(this->get_logger(), "MCU diagnostics started.");
   }
@@ -244,19 +246,20 @@ void ClearpathDiagnosticUpdater::firmware_diagnostic(DiagnosticStatusWrapper & s
  * @param delimiter Delimiter to split with
  * @return std::vector<std::string> Vector of split string
  */
-static std::vector<std::string> split(std::string s, std::string delimiter) {
-    size_t pos_start = 0, pos_end, delim_len = delimiter.length();
-    std::string token;
-    std::vector<std::string> res;
+static std::vector<std::string> split(std::string s, std::string delimiter)
+{
+  size_t pos_start = 0, pos_end, delim_len = delimiter.length();
+  std::string token;
+  std::vector<std::string> res;
 
-    while ((pos_end = s.find(delimiter, pos_start)) != std::string::npos) {
-        token = s.substr(pos_start, pos_end - pos_start);
-        pos_start = pos_end + delim_len;
-        res.push_back(token);
-    }
+  while ((pos_end = s.find(delimiter, pos_start)) != std::string::npos) {
+    token = s.substr(pos_start, pos_end - pos_start);
+    pos_start = pos_end + delim_len;
+    res.push_back(token);
+  }
 
-    res.push_back(s.substr(pos_start));
-    return res;
+  res.push_back(s.substr(pos_start));
+  return res;
 }
 
 /**
@@ -264,47 +267,35 @@ static std::vector<std::string> split(std::string s, std::string delimiter) {
  */
 void ClearpathDiagnosticUpdater::firmware_alerts_diagnostic(DiagnosticStatusWrapper & stat)
 {
-  if (mcu_alerts_msg_.data.empty() || mcu_alerts_msg_.data == "None")
-  {
+  if (mcu_alerts_msg_.data.empty() || mcu_alerts_msg_.data == "None") {
     stat.summary(DiagnosticStatus::OK, "No firmware alerts reported");
-  }
-  else
-  {
+  } else {
     unsigned char diagnostic_status = DiagnosticStatus::OK;
     std::vector<std::string> alerts = split(mcu_alerts_msg_.data, ",");
 
-    for (const auto &a : alerts)
-    {
+    for (const auto & a : alerts) {
       std::string alert_title = "Firmware Alert " + a;
       std::string alert_message;
       unsigned char alert_status = DiagnosticStatus::OK;
-      try
-      {
+      try {
         alert_message = DiagnosticLabels::FIRMWARE_ALERTS.at(a)[0];
         // Add the troubleshooting message if it exists
-        if(DiagnosticLabels::FIRMWARE_ALERTS.at(a)[1].size() > 1)
-        {
+        if (DiagnosticLabels::FIRMWARE_ALERTS.at(a)[1].size() > 1) {
           alert_message += ": " + DiagnosticLabels::FIRMWARE_ALERTS.at(a)[1];
         }
-      }
-      catch (const std::out_of_range &)
-      {
+      } catch (const std::out_of_range &) {
         alert_message = "Unknown firmware alert code";
       }
       stat.add(alert_title, alert_message);
 
       // Warning for W alerts, Error for E alerts
-      if (a.find('W') != std::string::npos)
-      {
+      if (a.find('W') != std::string::npos) {
         alert_status = DiagnosticStatus::WARN;
-      }
-      else if (a.find('E') != std::string::npos)
-      {
+      } else if (a.find('E') != std::string::npos) {
         alert_status = DiagnosticStatus::ERROR;
       }
 
-      if (alert_status > diagnostic_status)
-      {
+      if (alert_status > diagnostic_status) {
         diagnostic_status = alert_status;
       }
     }
