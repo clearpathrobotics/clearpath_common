4ddd5a949736fc5ccea0725ab9828769f164ee3f
Luis Camero <lcamero@clearpathrobotics.com>
2026-01-06 16:38:21 -0500

    Make sample generation callable from ros2cli

--- clearpath_generator_common/test/test_generate_samples.py
+++ clearpath_generator_common/test/test_generate_samples.py
@@ -1,5 +1,36 @@
+# Software License Agreement (BSD)
+#
+# @author    Luis Camero <lcamero@clearpathrobotics.com>
+# @copyright (c) 2026, Clearpath Robotics, Inc., All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+# * Redistributions of source code must retain the above copyright notice,
+#   this list of conditions and the following disclaimer.
+# * Redistributions in binary form must reproduce the above copyright notice,
+#   this list of conditions and the following disclaimer in the documentation
+#   and/or other materials provided with the distribution.
+# * Neither the name of Clearpath Robotics nor the names of its contributors
+#   may be used to endorse or promote products derived from this software
+#   without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+# LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+# CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+# SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+# INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+# CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+# ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
+
+# Redistribution and use in source and binary forms, with or without
+# modification, is not permitted without the express permission
+# of Clearpath Robotics.
+
 """Scripts to generate samples using the clearpath_generator_common."""
-import argparse
 import os
 import shutil
 import signal
@@ -120,7 +151,7 @@ def generate_semantic_description(setup_path: str) -> bool:
         '--output', os.path.join(setup_path, 'robot.srdf'),
         '--trials', '10000',
         '--min-collision-fraction', '0.95',
-        '--ros-args', '--log-level', 'fatal'
+        '--ros-args', '--log-level', 'info'
     ]
     ret, out, err = run_ros2_executable(path=path, argv=argv)
     # Delete pseudo package
@@ -210,32 +241,3 @@ def generate_test_samples(root_dir: str):
                     f'{"\n".join(sample_errors)}',
             errors=sample_errors
         )
-
-
-def main():
-    """Generate all files in directory path from argument."""
-    default_root_dir = os.path.join(
-        os.path.dirname(
-            os.path.dirname(
-                os.path.realpath(__file__))),
-        'samples')
-    # Get Root Directory from Args
-    parser = argparse.ArgumentParser(
-        prog='Clearpath Common Sample Generator',
-        description='Generate all common files from test samples in the clearpath_config package.',
-    )
-    parser.add_argument(
-        '--out',
-        help='Output directory of generated files.',
-        default=default_root_dir,
-        required=False)
-    args = parser.parse_args()
-
-    root_dir = os.path.abspath(args.out)
-    assert os.path.isdir(root_dir), f'Output directory "{root_dir}" does not exist.'
-
-    generate_test_samples(root_dir)
-
-
-if __name__ == '__main__':
-    main()
