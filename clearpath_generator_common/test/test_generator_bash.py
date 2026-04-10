c5c9448f3085fde77ba559362eeab68b668a90c7
Luis Camero <lcamero@clearpathrobotics.com>
2026-04-10 14:08:38 -0400

    Use pathlib to find extension

--- clearpath_generator_common/test/test_generator_bash.py
+++ clearpath_generator_common/test/test_generator_bash.py
@@ -26,6 +26,7 @@
 # ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 # POSSIBILITY OF SUCH DAMAGE.
 import os
+import pathlib
 import shutil
 
 from ament_index_python.packages import get_package_share_directory
@@ -43,7 +44,7 @@ class TestRobotLaunchGenerator:
         share_dir = get_package_share_directory('clearpath_config')
         sample_dir = os.path.join(share_dir, 'sample')
         for sample in os.listdir(sample_dir):
-            if os.path.splitext(sample)[-1].lower() != '.yaml':
+            if pathlib.Path(sample).suffix != '.yaml':
                 continue
             # Create Clearpath Directory
             src = os.path.join(sample_dir, sample)
